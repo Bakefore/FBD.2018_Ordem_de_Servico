7e5b3e3df126cebb866f58dde85bef759c35963b
Bakefore <44624532+Bakefore@users.noreply.github.com>
2018-11-14 18:00:50 -0200

    Add files via upload
    Inserindo Empresa

--- Sistema_de_Ordem_de_Servico/php/DAO/EmpresaDAO.php
+++ Sistema_de_Ordem_de_Servico/php/DAO/EmpresaDAO.php
@@ -1,19 +1,40 @@
 <?php  
-	require_once("php/autoload/autoloadModel.php");
+	require_once("../autoload/autoloadModel.php");
 	class EmpresaDAO{
 		private $empresa;
+		private $idEndereco;
 
-		public function __construct($empresa){
+		public function __construct($empresa, $idEndereco){
 			$this->empresa = $empresa;
+			$this->idEndereco = $idEndereco;
 		}
 
 		public function getEmpresa(){
 			return $this->empresa;
 		}
 
+		public function getIdEndereco(){
+			return $this->idEndereco;
+		}
+
 		public function cadastrar(){
 			$sql = new Sql();
-			$sql->query("");
+
+			$resultadoEmpresa = $sql->select("select * from empresa where razaoSocial = :razaoSocial and nomeFantasia = :nomeFantasia and cnpj = :cnpj", array(
+				":razaoSocial"=>$this->empresa->getRazaoSocial(),
+				":nomeFantasia"=>$this->empresa->getNomeFantasia(),
+				":cnpj"=>$this->empresa->getCNPJ()
+			));
+
+			if($resultadoEmpresa == null){
+				$sql->query("insert into empresa (razaoSocial, nomeFantasia, cnpj, idEndereco) values (:razaoSocial, :nomeFantasia, :cnpj, :idEndereco)", array(
+
+					":razaoSocial"=>$this->empresa->getRazaoSocial(), 
+					":nomeFantasia"=>$this->empresa->getNomeFantasia(), 
+					":cnpj"=>$this->empresa->getCNPJ(), 
+					":idEndereco"=>$this->getIdEndereco()
+				));	
+			}			
 		}
 	}
 ?>
